39f5cb4f719e3c18a95fbf953a104ad2cae687c6
ovedfs <ovedfs@gmail.com>
2022-09-08 22:45:34 -0500

    Sistema de tickets con autenticación con Sanctum, autorización con Spatie y Notificaciones

---
+++ app/Http/Controllers/Api/ArrendadorTicketController.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Models\Ticket;
+use App\Models\Contract;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Notifications\NewTicketNotification;
+
+class ArrendadorTicketController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('can:tickets.index')->only('index');
+        $this->middleware('can:tickets.store')->only('store');
+        $this->middleware('can:tickets.show')->only('show');
+        $this->middleware('can:tickets.update')->only('update');
+        $this->middleware('can:tickets.destroy')->only('destroy');
+        $this->middleware('can:tickets.close')->only('close');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Contract $contract)
+    {
+        if(!(auth()->user()->id === $contract->user->id)) {
+            return response()->json([
+                'message' => 'No autorizado'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Listado de tickets',
+            'tickets' => $contract->tickets
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Contract $contract)
+    {
+        if(!(auth()->user()->id === $contract->user->id)) {
+            return response()->json([
+                'message' => 'No autorizado'
+            ]);
+        }
+
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'image' => 'sometimes|image',
+        ]);
+
+        $abogado = User::role('abogado')->get()->random();
+
+        $ticket = $contract->tickets()->create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'arrendador_id' => $contract->user_id,
+            'abogado_id' => $abogado->id,
+        ]);
+
+        auth()->user()->notify(new NewTicketNotification([
+            'message' => 'Hemos recibido tu mensaje, en breve te daremos respuesta',
+            'ticket' => $ticket,
+            'url' => url("contracts/{$ticket->contract->id}/tickets/{$ticket->id}")
+        ]));
+
+        $abogado->notify(new NewTicketNotification([
+            'message' => 'Se te ha asignado un nuevo ticket',
+            'ticket' => $ticket,
+            'url' => url("abogado-tickets/{$ticket->id}")
+        ]));
+
+        return response()->json([
+            'message' => 'Ticket creado satisfactoriamente',
+            'ticket' => $ticket
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Ticket  $ticket
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Contract $contract, Ticket $ticket)
+    {
+        if(!(auth()->user()->id === $contract->user->id)) {
+            return response()->json([
+                'message' => 'No autorizado'
+            ]);
+        }
+
+        if(! ($contract->tickets->contains($ticket))) {
+            return response()->json([
+                'message' => 'No autorizado'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Ticket obtenido satisfactoriamente',
+            'ticket' => $ticket->load('ticketResponses')
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ticket  $ticket
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Contract $contract, Ticket $ticket)
+    {
+        if(!(auth()->user()->id === $contract->user->id)) {
+            return response()->json([
+                'message' => 'No autorizado'
+            ]);
+        }
+
+        if(! ($contract->tickets->contains($ticket))) {
+            return response()->json([
+                'message' => 'No autorizado'
+            ]);
+        }
+
+        if(! $ticket->open) {
+            return response()->json([
+                'message' => 'No autorizado, el ticket esta cerrado',
+            ]);
+        }
+
+        $request->validate([
+            'response' => 'required',
+            'image' => 'sometimes|image'
+        ]);
+
+        $ticketResponse = $ticket->ticketResponses()->create([
+            'response' => $request->response,
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return response()->json([
+            'message' => 'Respuesta agregada satisfactoriamente',
+            'ticket' => $ticket->load('ticketResponses')
+        ]);
+    }
+
+    public function close(Contract $contract, Ticket $ticket)
+    {
+        if(!(auth()->user()->id === $contract->user->id)) {
+            return response()->json([
+                'message' => 'No autorizado'
+            ]);
+        }
+
+        if(! ($contract->tickets->contains($ticket))) {
+            return response()->json([
+                'message' => 'No autorizado'
+            ]);
+        }
+
+        $ticket->open = false;
+        $ticket->save();
+
+        return response()->json([
+            'message' => 'Ticket cerrado satisfactoriamente',
+            'ticket' => $ticket->load('ticketResponses')
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,29 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\AbogadoTicketController;
+use App\Http\Controllers\Api\ArrendadorTicketController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ["auth:sanctum"]], function(){
+    Route::get('profile', [AuthController::class, 'profile']);
+    Route::get('logout', [AuthController::class, 'logout']);
+
+    // Arrendador --> tickets
+    Route::get('contracts/{contract}/tickets', [ArrendadorTicketController::class, 'index']);
+    Route::post('contracts/{contract}/tickets', [ArrendadorTicketController::class, 'store']);
+    Route::get('contracts/{contract}/tickets/{ticket}', [ArrendadorTicketController::class, 'show']);
+    Route::put('contracts/{contract}/tickets/{ticket}', [ArrendadorTicketController::class, 'update']);
+    Route::get('contracts/{contract}/tickets/{ticket}/close', [ArrendadorTicketController::class, 'close']);
+
+    // Abogado --> tickets
+    Route::get('abogado-tickets', [AbogadoTicketController::class, 'index']);
+    Route::post('abogado-tickets', [AbogadoTicketController::class, 'store']);
+    Route::get('abogado-tickets/{ticket}', [AbogadoTicketController::class, 'show']);
+    Route::put('abogado-tickets/{ticket}', [AbogadoTicketController::class, 'update']);
+    Route::get('abogado-tickets/{ticket}/close', [AbogadoTicketController::class, 'close']);
+    Route::delete('abogado-tickets/{ticket}', [AbogadoTicketController::class, 'destroy']);
 });
